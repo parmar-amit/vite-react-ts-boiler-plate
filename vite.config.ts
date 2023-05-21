7d73635d582adf8a7dcc8fda1c0077da350032aa
AMIT-PARMAR\amit_ <amit_p89@hotmail.com>
2023-05-21 16:10:08 +0530

    added path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from "node:path";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Absolute imports
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@style": path.resolve(__dirname, "./src/style"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@services": path.resolve(__dirname, "./src/services"),
+    },
+  },
 });
